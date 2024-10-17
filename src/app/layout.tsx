cf3eff334cd6ba765d3af18af7fe3c376d927096
raigrc <ravenjstn.grc@gmail.com>
2024-10-17 19:30:21 +0800

    comment out unused fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,16 +7,17 @@ import React from "react";
 const poppins = localFont({
   src: "./fonts/poppins-v21-latin-regular.woff2",
 });
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
