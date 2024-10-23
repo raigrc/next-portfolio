2c22ef119687c275e24ce76d7dbab5eca6616a18
raigrc <ravenjstn.grc@gmail.com>
2024-10-23 13:56:13 +0800

    added see more feature

--- src/components/main/projects/description.tsx
+++ src/components/main/projects/description.tsx
@@ -1,9 +1,23 @@
-import React from "react";
+"use client";
+import { Button } from "@/components/ui/button";
+import React, { useState } from "react";
 
 const Description = ({ description }: { description: string }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleDescription = () => {
+    setIsExpanded(!isExpanded);
+  };
   return (
-    <div className="overflow-hidden text-ellipsis h-20 py-4 text-sm text-muted-foreground">
-      {description}
+    <div className="py-4 text-sm text-muted-foreground">
+      <div
+        className={`${isExpanded ? "h-auto" : "h-20"} overflow-hidden text-ellipsis`}
+      >
+        {description}
+      </div>
+      <Button variant="link" onClick={toggleDescription} className="px-0">
+        {isExpanded ? "See less" : "See more"}
+      </Button>
     </div>
   );
 };
