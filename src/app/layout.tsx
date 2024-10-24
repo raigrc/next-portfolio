46b0c9b955ca81951f4ed0db6c43d25a356dd456
raigrc <ravenjstn.grc@gmail.com>
2024-10-24 14:50:56 +0800

    fix overflow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
       >
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           {/* BACKGROUND SNIPPET */}
-          <div className="relative h-full w-full bg-background">
+          <div className="relative h-full w-full overflow-x-clip bg-background">
             <div className="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
             <div className="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
           </div>
